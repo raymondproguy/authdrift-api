f20dc8ffd1ca659d55578fec33af840b7a0af69b
raymondproguy25 <raymondnicholas833@gmail.com>
2025-09-10 16:48:26 +0100

    fix: added project to route

--- app/controllers/project.controller.ts
+++ app/controllers/project.controller.ts
@@ -1,33 +1,33 @@
 // @desc Get projects 
-// @route GET /api/v2
+// @route GET /api/v2/project
 // @access private
 export const getProject = (req, res) => {
   res.status(200).json({ message: "Get project."}) 
 };
 
 // @desc Post project
-// @route POST /api/v2
+// @route POST /api/v2/project
 // @access private
-export const postProject = (req, res) => {
+export const createProject = (req, res) => {
   res.status(201).json({ message: "Set projects."})
 };
 
 // @desc Get projects
-// @route GET /api/v2/:id
+// @route GET /api/v2/project/:id
 // @access private
 export const getProjects = (req, res) => {
   res.status(200).json({ message: "Get projects."})
 };
 
 // @desc Update project
-// @route PUT /api/v2/:id
+// @route PUT /api/v2/project/:id
 // @access private
 export const updateProject = (req, res) => {
   res.status(200).json({ message: "Update projects."})
 };
 
 // @desc Delete project
-// @route DELETE /api/v2/:id
+// @route DELETE /api/v2/project/:id
 // @access private
 export const deleteProject = (req, res) => {
   res.status(200).json({ message: "Delete projects."})
