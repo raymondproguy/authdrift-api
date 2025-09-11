3ee25351ab9dc3a6ae40d34752a7ae06aef92138
raymondproguy25 <raymondnicholas833@gmail.com>
2025-09-12 00:18:16 +0100

    refactor: Moved bussenees logic to services

---
+++ app/services/project.service.ts
@@ -0,0 +1,24 @@
+// ./app/services/project.service.ts
+
+import Project from "../models/project.model";
+
+export const createProject = async (data) => {
+  const project = new Project(data);
+  return await project.save();
+};
+
+ export const getAllProject = async () =>{
+  return await Project.find();
+};
+
+export const getAllProjectById = async (id) =>{
+  return await Project.findById(id);
+};
+
+export const updateProject = async (id, data) =>{
+  return await Project.findByIdAndUpdate(id, data, { new: true });
+};
+
+export const deleteProject = async (id) =>{
+  return await Project.findByIdAndDelete(id);
+};
