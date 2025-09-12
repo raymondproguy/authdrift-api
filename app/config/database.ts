3157bc5b22f235c411468d2e9353298ea29e6e87
raymondproguy25 <raymondnicholas833@gmail.com>
2025-09-12 16:03:39 +0100

    feat: implemented logger and colors in database file

--- app/config/database.ts
+++ app/config/database.ts
@@ -1,13 +1,14 @@
 // src/config/database.ts
 import mongoose from "mongoose";
+import { logger } from "../utils/logger";
 
 const connectDB = async() => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI!);
-    console.log(`MongoDB Connected:
-                ${conn.connection.host}`);
-  } catch (error) {
-    console.log("MongoDB connection failed:", error);
+    logger.success(`MongoDB Connected:
+                ${conn.connection.host}` .green);
+  } catch (error: any) {
+    logger.error(`MongoDB connection failed:, ${error.message}` .red);
      process.exit(1);
   }
 };
