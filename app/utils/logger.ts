c8471705265883ac3817c1ae427d4d7490c8a0ee
raymondproguy25 <raymondnicholas833@gmail.com>
2025-09-12 11:44:35 +0100

    feat: added logger to utils and used colors for colorful console text

---
+++ app/utils/logger.ts
@@ -0,0 +1,21 @@
+import 'colors';
+
+export const logger = {
+  info: (message: string) => {
+    console.log(`[INFO] ${new Date().toISOString()}: ${message}`.blue);
+  },
+  success: (message: string) => {
+    console.log(`[SUCCESS] ${new Date().toISOString()}: ${message}`.green);
+  },
+  warn: (message: string) => {
+    console.log(`[WARN] ${new Date().toISOString()}: ${message}`.yellow);
+  },
+  error: (message: string) => {
+    console.log(`[ERROR] ${new Date().toISOString()}: ${message}`.red);
+  },
+  debug: (message: string) => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(`[DEBUG] ${new Date().toISOString()}: ${message}`.magenta);
+    }
+  }
+};
