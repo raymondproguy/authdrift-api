db62a007ad7c6f8f07e5c4a0659df4a3982ce7de
raymondproguy25 <raymondnicholas833@gmail.com>
2025-09-20 10:49:53 +0100

    feat: Implemented api key route and added it to dev router

---
+++ app/routers/apiKey.route.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  handleGenerateApiKey,
+  handleGetApiKeys,
+  handleRevokeApiKey
+} from "../controllers/apiKey.controller";
+
+const router = express.Router();
+
+// All routes are under /api/v2/dev/:devId/api-keys
+router.route("/")
+  .get(handleGetApiKeys)          // GET /api/v2/dev/:devId/api-keys
+  .post(handleGenerateApiKey);    // POST /api/v2/dev/:devId/api-keys
+
+router.route("/:keyId")
+  .delete(handleRevokeApiKey);    // DELETE /api/v2/dev/:devId/api-keys/:keyId
+
+export default router;
--- app/routers/dev.route.ts
+++ app/routers/dev.route.ts
@@ -1,3 +1,5 @@
+// ./app/routers/apiKey.route.ts
+
 import express from "express";
 import {
   handleCreateDev,
@@ -5,6 +7,7 @@ import {
   handleUpdateDev,
   handleDeleteDev,
 } from "../controllers/dev.controller";
+import ApiKeyRoutes from "../routers/apiKey.route";
 //import { validateDev } from "../middleware/validation";
 
 const router = express.Router();
@@ -18,4 +21,5 @@ router.route("/:id")
   .put(handleUpdateDev)
   .delete(handleDeleteDev);
 
+router.use("/:devID/api-keys", ApiKeyRoutes)
 export default router;
