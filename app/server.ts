409ee8313c1b870a963a05971dd20a41c74e62bb
raymondproguy25 <raymondnicholas833@gmail.com>
2025-09-20 10:12:31 +0100

    feat: Added the dev routes to server file

--- app/server.ts
+++ app/server.ts
@@ -14,6 +14,7 @@ import helmet from "helmet";
 import rateLimit from 'express-rate-limit';
 import { logger } from './utils/logger';
 import projectRoutes from "./routers/project.route";
+import devRoutes from "./routers/dev.route"
 import { requestLogger } from './middleware/request.logger';
 //import dashboardRoute from "./routes/dashboard.route.ts";
 //import { setupSwaggerDocs } from "./config/swagger.ts";
@@ -36,7 +37,7 @@ app.use(requestLogger);
 //app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use("/api/v2/project", projectRoutes);
-//app.use("/api", dashboardRoute);
+app.use("/api/v2/auth", devRoutes);
 
 //setupSwaggerDocs(app);
 
