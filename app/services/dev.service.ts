d417f1f9719c3a9427ad263424bc5bcbc085060b
raymondproguy <raymondnicholas833@gmail.com>
2025-09-21 15:40:09 +0100

    feat: Implemented more security added jwt and bcrypt

--- app/services/dev.service.ts
+++ app/services/dev.service.ts
@@ -1,6 +1,8 @@
 // ./app/services/dev.service.ts
 
 import Dev from "../models/dev.model";
+import bcrypt from "bcryptjs";
+import jwt  from "jsonwebtoken";
 import { logger } from "../utils/logger";
 
 export const createDev = async (data: any) => {
@@ -43,4 +45,55 @@ export const deleteDev = async (id: string) => {
   const dev = await Dev.findByIdAndDelete(id);
   logger.debug(`Dev deleted from DB: ${id}`);
   return dev;
+};
+
+export const registerDev = async (data:any) =>{
+  const existingDev = await Dev.findOne({
+    $or: [{email: data.email}, {devname: data.devname}]
+  });
+  if(existingDev) {
+    throw new Error ("Dev already exits with this email or username ");
+  }
+  const dev = new Dev(data);
+  const savedDev = await dev.save();
+  logger.debug(`Dev registerd ${savedDev.email}`)
+
+  //Generate JWT token 
+  const token = generateJWT(savedDev._id);
+
+  return {dev: savedDev, token}
+};
+
+const loginDev = async (email:string, password:string) =>{
+  const dev = await Dev.findOne({email}).select("+password")
+  if (!dev){
+    throw new Error ("Invalid credentials");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, dev.password);
+  if (!isPasswordValid) {
+    throw new Error ("Invalid credentials");
+  }
+  logger.debug(`Logged in dev ${dev.email}`);
+
+  const token = generateJWT(dev._id);
+
+  return {dev, token};
+};
+
+export const generateJWT = (devId:string) =>{
+  return jwt.sign(
+    {id: devId},
+    process.env.JWT_SECRET || "fallback-secret",
+    {expiresin: "7d"}
+  );
+};
+
+export const verifyToken = (token:string) =>{
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "fallback-secret");
+    return decoded;
+  } catch (error) {
+    throw new Error("Invalid token");
+  };
 };
