d9c9f8379a8783ffd92e0b74c21762bc8826d380
raymondproguy <raymondnicholas833@gmail.com>
2025-09-22 18:13:04 +0100

    feat: Implemented the login and register request in controller

--- app/controllers/dev.controller.ts
+++ app/controllers/dev.controller.ts
@@ -5,9 +5,87 @@ import {
   createDev,
   getDevByName,
   updateDev,
-  deleteDev
+  deleteDev,
+  registerDev,
+  loginDev
 } from "../services/dev.service";
 import { logger } from "../utils/logger";
+
+export const handleRegister = async(req:Request, res:Response) =>{
+  try {
+    const {dev, token} = await registerDev(req.body);
+    logger.success(`Dev registered: ${dev.email}`);
+    res.status(201).json({
+      success: true,
+      data:{
+        _id: dev._id,
+        email: dev.email,
+        devname: dev.devname,
+        company: dev.company,
+        isActive: dev.isActive,
+        createdAt: dev.createdAt
+      },
+      token,
+      message: "Registion successful"
+    });
+  } catch (error: any) {
+    logger.error(`Register error: ${error.message}`);
+    res.status(400).json({
+      success: false,
+      error:error.message
+    });
+  }
+};
+
+export const handleLogin = async (req:Request, res:Response) =>{
+  try {
+    const {email,password} = req.body;
+     const {dev, token} = await loginDev(email,password);
+    logger.success(`Dev Logged in: ${dev.email}`);
+    res.status(201).json({
+      success: true,
+      data:{
+        _id: dev._id,
+        email: dev.email,
+        devname: dev.devname,
+        company: dev.company,
+        isActive: dev.isActive,
+        createdAt: dev.createdAt
+      },
+      token,
+      message: "Login successful"
+    });
+  } catch (error: any) {
+    logger.error(`Login error: ${error.message}`);
+    res.status(400).json({
+      success: false,
+      error:error.message
+    });
+  }
+};
+
+export const handleLoginGetProfile = async (req:Request, res:Response) =>{
+  try {
+    const dev = await getDevByName((req as any).dev.devname);
+    if(!dev) {
+      return res.status(404).json({
+        success: false,
+        error: "Dev not found"
+      });
+    }
+    logger.info(`Profile for Dev: ${dev.devname}`);
+    res.status(200).json({
+      success: true,
+      data: dev
+    });
+  } catch (error: any) {
+    logger.error(`Get profile error: ${error.message}`);
+    res.status(400).json({
+      success: false,
+      error:error.message
+    });
+  }
+};
 
 export const handleCreateDev = async (req: Request, res: Response) => {
   try {
