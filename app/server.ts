53c7b6e92051db8fa8cbec59cf8b79ec9eb90674
raymondproguy <raymondnicholas833@gmail.com>
2025-09-27 21:18:25 +0100

    feat: Implement the dev oath endpoint in the serverfile

--- app/server.ts
+++ app/server.ts
@@ -5,16 +5,19 @@
  * Free, Open Source, Secure Authentication API
  * Just plug and use no need for extra setup
  */
-import express from 'express';
+import express from "express";
 import { Request, Response } from "express";
 import connectDB from './config/database';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import helmet from "helmet";
-import rateLimit from 'express-rate-limit';
-import { logger } from './utils/logger';
+import rateLimit from "express-rate-limit";
+import passport from "passport";
+import session  from "express-session";
+import { logger } from "./utils/logger";
 import projectRoutes from "./routers/project.route";
 import devRoutes from "./routers/dev.route";
 import userRoutes from "../app/routers/user.route";
+import devOathRoute from "./routers/oauth.route";
 import { requestLogger } from './middleware/request.logger';
 //import dashboardRoute from "./routes/dashboard.route.ts";
 //import { setupSwaggerDocs } from "./config/swagger.ts";
@@ -31,17 +34,32 @@ app.use(rateLimit({
   max: 100
 }));
 
+// Add session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'fallback-secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(requestLogger);
 
 app.use("/api/v2/project", projectRoutes);
 app.use("/api/v2/dev", devRoutes);
 app.use("/api/v2/user", userRoutes);
 
+//dev oauth endpoint
+app.use("/api/v2/dev-auth", devOathRoute);
+
 //setupSwaggerDocs(app);
 
 app.get('/', (req:Request, res:Response) => {
   logger.info("Root endpoint accessed")
-  res.json({ message: 'Welcome to AuthDrift API V2, Secure your fueture with AuthifyNG. Simple, modern, secure Authentication system for your app/startup '});
+  res.json({ message: 'Welcome to AuthDrift API V2, Secure your fueture with AuthDrift. Simple, modern, secure Authentication system for your app/startup '});
 });
 
 app.get('/health', (req:Request, res:Response) => {
